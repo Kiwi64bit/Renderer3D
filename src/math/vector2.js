7f1681168456dfd471c6a2e269205bbc5b08994f
Kiwi64bit <207353425+Kiwi64bit@users.noreply.github.com>
2026-06-26 00:51:39 +0300

    feat(math): extend Vector2 and Vector3 classes

--- src/math/vector2.js
+++ src/math/vector2.js
@@ -79,6 +79,100 @@ class Vector2 {
 
         return this;
     }
+
+    /**
+     * Negates this Vector2 components.
+     *
+     * @returns {this} This Vector2 for chaining.
+     */
+    negate() {
+        this.x = -this.x;
+        this.y = -this.y;
+
+        return this;
+    }
+
+    /**
+     * Compares this Vector2 with another Vector2.
+     *
+     * @param {Vector2} other - The other Vector2.
+     * @returns {boolean} Whether the Vectors are equal or not.
+     */
+    equals(other) {
+        return this.x === other.x && this.y === other.y;
+    }
+
+    /**
+     * Calculates the squared magnitude of this Vector2.
+     *
+     * @readonly
+     * @type {number}
+     */
+    get magnitudeSquared() {
+        return this.x * this.x + this.y * this.y;
+    }
+
+    /**
+     * Calculates the magnitude (length) of this Vector2.
+     *
+     * @readonly
+     * @type {number}
+     */
+    get magnitude() {
+        return Math.sqrt(this.magnitudeSquared);
+    }
+
+    /**
+     * Converts this Vector2 to a unit vector
+     * (A vector in the same direction but with a magnitude of 1).
+     *
+     * @return {Vector2} This Vector2 for chaining.
+     */
+    normalize() {
+        return this.div(this.magnitude || 1);
+    }
+
+    /**
+     * Calculates the dot product between this Vector2 and another Vector2.
+     *
+     * @param {Vector2} other - The other Vector2.
+     * @returns {number} The dot product.
+     */
+    dot(other) {
+        return this.x * other.x + this.y * other.y;
+    }
+
+    /**
+     * Calculate the cross product between this Vector2 and another Vector2.
+     *
+     * @param {Vector2} other - The other Vector2.
+     * @returns {number} The cross product.
+     */
+    cross(other) {
+        return this.x * other.y - this.y * other.x;
+    }
+
+    /**
+     * Calculate the squared distance between this Vector2 and another Vector2.
+     *
+     * @param {Vector2} other - The other Vector2.
+     * @returns {number} The squared distance.
+     */
+    distanceSquared(other) {
+        const dx = this.x - other.x;
+        const dy = this.y - other.y;
+        return dx * dx + dy * dy;
+    }
+
+    /**
+     * Calculate the distance between this Vector2 and another Vector2.
+     *
+     * @param {Vector2} other - The other Vector2.
+     * @returns {number} The distance.
+     */
+    distance(other) {
+        return Math.sqrt(this.distanceSquared(other));
+    }
 }
 
 export { Vector2 };
--- src/math/vector3.js
+++ src/math/vector3.js
@@ -91,6 +91,107 @@ class Vector3 {
 
         return this;
     }
+
+    /**
+     * Negates this Vector3 components.
+     *
+     * @returns {Vector3} This Vector3 for chaining.
+     */
+    negate() {
+        this.x = -this.x;
+        this.y = -this.y;
+        this.z = -this.z;
+
+        return this;
+    }
+
+    /**
+     * Compares this Vector3 with another Vector3.
+     *
+     * @param {Vector3} other - The other Vector3.
+     * @returns {boolean} Whether the Vectors are equal or not.
+     */
+    equals(other) {
+        return this.x === other.x && this.y === other.y && this.z === other.z;
+    }
+
+    /**
+     * Calculates the squared magnitude of this Vector3.
+     *
+     * @readonly
+     * @type {number}
+     */
+    get magnitudeSquared() {
+        return this.x * this.x + this.y * this.y + this.z * this.z;
+    }
+
+    /**
+     * Calculates the magnitude (length) of this Vector3.
+     *
+     * @readonly
+     * @type {number}
+     */
+    get magnitude() {
+        return Math.sqrt(this.magnitudeSquared);
+    }
+
+    /**
+     * Converts this Vector3 to a unit vector
+     * (A vector in the same direction but with a magnitude of 1).
+     *
+     * @returns {Vector3} This Vector3 for chaining.
+     */
+    normalize() {
+        return this.div(this.magnitude || 1);
+    }
+
+    /**
+     * Calculates the dot product between this Vector3 and another Vector3.
+     *
+     * @param {Vector3} other - The other Vector3.
+     * @returns {number} The dot product.
+     */
+    dot(other) {
+        return this.x * other.x + this.y * other.y + this.z * other.z;
+    }
+
+    /**
+     * Calculates the cross product between this Vector3 and another Vector3.
+     *
+     * @param {Vector3} other - The other Vector3.
+     * @returns {Vector3} The cross product.
+     */
+    cross(other) {
+        return new Vector3(
+            this.y * other.z - this.z * other.y,
+            this.z * other.x - this.x * other.z,
+            this.x * other.y - this.y * other.x,
+        );
+    }
+
+    /**
+     * Calculates the squared distance between this Vector3 and another Vector3.
+     *
+     * @param {Vector3} other - The other Vector3.
+     * @returns {number} The squared distance.
+     */
+    distanceSquared(other) {
+        const dx = this.x - other.x;
+        const dy = this.y - other.y;
+        const dz = this.z - other.z;
+
+        return dx * dx + dy * dy + dz * dz;
+    }
+
+    /**
+     * Calculates the distance between this Vector3 and another Vector3.
+     *
+     * @param {Vector3} other - The other Vector3.
+     * @returns {number} The distance.
+     */
+    distance(other) {
+        return Math.sqrt(this.distanceSquared(other));
+    }
 }
 
 export { Vector3 };
